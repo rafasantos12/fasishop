a71be340612caa89fdb686442f170d85c1ef025e
rafasantos12 <ssouzarafaelxd@gmail.com>
2022-09-11 21:43:13 -0400

    commit

---
+++ FRONT/src/components/partials/Footer/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterArea = styled.div`
+    height:100px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    text-align:center;
+    color:#000000;
+    font-size:14px;
+    border-top:1px solid #CCC;
+    margin-top:120px;
+    background-color:#EEE
+`;
