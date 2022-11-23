5d61f945cf29565ee39d14e989560aa1aad68b21
Rafael Santos <102121949+rafasantos12@users.noreply.github.com>
2022-11-23 10:21:53 -0400

    Add files via upload

---
+++ fasishop/PROJETO INTERDISCIPLINAR 2/FRONT/src/components/partials/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FooterArea } from './styled';
+
+const Footer = () => {
+    return (
+        <FooterArea>
+            Desenvolvido por Rafas Group<br/>
+            
+        </FooterArea>
+    );
+}
+
+export default Footer;
